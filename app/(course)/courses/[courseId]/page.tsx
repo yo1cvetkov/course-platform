b9de72e675493f80e0e5a3fda154be7437e2d67d
Jovan Cvetkov <yo1.cvetkov@gmail.com>
2023-10-05 00:48:20 +0200

    Implemented layout for the course with the chapters sidebar

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,3 @@
+export default function CourseIdPage() {
+  return <div>Watch the course</div>;
+}
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -14,7 +14,7 @@ export function NavbarRoutes() {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayerPage = pathname?.includes("/chapter");
+  const isPlayerPage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
   return (
