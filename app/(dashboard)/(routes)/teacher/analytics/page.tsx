db68e0045598cff6788b05c6f1dc9aa97bda0ed6
Jovan Cvetkov <yo1.cvetkov@gmail.com>
2023-10-05 14:34:32 +0200

    Added the analytics page for the teacher user

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,3 +1,25 @@
-export default function AnalyticsPage() {
-  return <div className="pt-20">Analytics page</div>;
+import { auth } from "@clerk/nextjs";
+
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
+
+export default async function AnalyticsPage() {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Sales" value={totalSales} />
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 }
